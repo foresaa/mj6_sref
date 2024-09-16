37bd21d5c8a6603442d93feeba3e5636b57ee00d
foresaa <133493061+foresaa@users.noreply.github.com>
2024-09-16 22:53:34 +0100

    Update main.py
    took out duplication

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 from app.api.v1 import endpoints
 from app.db.base import Base
@@ -30,36 +29,3 @@ app.mount("/images", StaticFiles(directory="uploads"), name="images")
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
-=======
-from fastapi import FastAPI
-from app.api.v1 import endpoints
-from app.db.base import Base
-from app.db.session import engine
-import jwt
-from dotenv import load_dotenv
-import os
-from fastapi.staticfiles import StaticFiles
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY")
-
-# Example for encoding a token
-token = jwt.encode({"some": "payload"}, SECRET_KEY, algorithm="HS256")
-
-# Example for decoding a token
-decoded = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-
-# Initialize FastAPI app
-app = FastAPI()
-
-# Include API routes from the endpoints module
-app.include_router(endpoints.router)
-
-# Mount the /images directory
-app.mount("/images", StaticFiles(directory="uploads"), name="images")
-
-# Create database tables
-Base.metadata.create_all(bind=engine)
->>>>>>> 31360ec005fc4f95e882871513d4899309140528
