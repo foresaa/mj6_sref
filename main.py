e5f4ef834ec633a9cb932cec7194c7a6c5b85f5d
foresaa <133493061+foresaa@users.noreply.github.com>
2024-09-16 21:31:02 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,25 +5,27 @@ from app.db.session import engine
 import jwt
 from dotenv import load_dotenv
 import os
+from fastapi.staticfiles import StaticFiles
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-
 # Example for encoding a token
-token = jwt.encode({"some": "payload"}, "your-secret-key", algorithm="HS256")
+token = jwt.encode({"some": "payload"}, SECRET_KEY, algorithm="HS256")
 
 # Example for decoding a token
-decoded = jwt.decode(token, "your-secret-key", algorithms=["HS256"])
-
+decoded = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
 
 # Initialize FastAPI app
 app = FastAPI()
 
 # Include API routes from the endpoints module
 app.include_router(endpoints.router)
+
+# Mount the /images directory
+app.mount("/images", StaticFiles(directory="uploads"), name="images")
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
